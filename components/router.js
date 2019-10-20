1c4f1aee7978c7261344d8bc37eae01268e95faf
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2019-10-20 15:35:53 +0300

    init express

---
+++ components/router.js
@@ -0,0 +1,45 @@
+const { createRouter } = require('express-object-router');
+
+// const CHECK_ACCESS = ({
+//   req,
+//   res,
+//   next,
+//   pass,
+//   props: { allowAccess },
+// }) => {
+//   if (allowAccess.includes(roles.EVERYONE)) {
+//     pass();
+
+//     return;
+//   }
+
+//   if (allowAccess.includes(roles.USER)) {
+//     superlogin.requireAuth(req, res, next);
+
+//     return;
+//   }
+
+//   throw FORBIDDEN();
+// };
+
+
+const router = createRouter({
+  pathsRelateTo: __dirname,
+  routesPaths: [
+    '../../app/components/**/*.router.js',
+  ],
+  middlewaresSequence: ({
+    PARAMS_VALIDATION,
+    QUERY_VALIDATION,
+    BODY_VALIDATION,
+    ROUTER_MIDDLEWARES,
+  }) => [
+    PARAMS_VALIDATION,
+    QUERY_VALIDATION,
+    BODY_VALIDATION,
+    // CHECK_ACCESS,
+    ROUTER_MIDDLEWARES,
+  ],
+});
+
+module.exports = router;
